5edba6982626c382b02465e561e5401c8bb8f2bc
mhalaabidi <99919693+mhalaabidi@users.noreply.github.com>
2023-06-24 13:53:54 +0100

    Update AuthenticationMicroserviceApplication.java

--- authentication-service/src/main/java/com/example/authenticationmicroservice/AuthenticationMicroserviceApplication.java
+++ authentication-service/src/main/java/com/example/authenticationmicroservice/AuthenticationMicroserviceApplication.java
@@ -36,7 +36,7 @@ public class AuthenticationMicroserviceApplication {
 			roleService.saveRole(Role.builder().name("USER").build());
 
 			// init users list
-			userService.saveUser( User.builder().email("faiez.abid@esprit.tn").password("mlaabidi").username("mlaabidi").roles(new  ArrayList<>()).build()) ;
+			userService.saveUser( User.builder().email("mhamed.laabidi@esprit.tn").password("mlaabidi").username("mlaabidi").roles(new  ArrayList<>()).build()) ;
 
 
 		} ;
